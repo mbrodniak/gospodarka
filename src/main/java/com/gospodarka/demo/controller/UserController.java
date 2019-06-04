3d52a930df98bf1b36fc8f65fdf2442cd5ac71bd
mbrodniak <47107265+mbrodniak@users.noreply.github.com>
2019-06-04 22:25:42 +0200

    auth improved

--- src/main/java/com/gospodarka/demo/controller/UserController.java
+++ src/main/java/com/gospodarka/demo/controller/UserController.java
@@ -2,13 +2,17 @@ package com.gospodarka.demo.controller;
 
 import com.gospodarka.demo.dto.UserDTO;
 import com.gospodarka.demo.entity.User;
+import com.gospodarka.demo.model.UserPrincipal;
 import com.gospodarka.demo.repository.UserRepository;
+
+import java.security.Principal;
 import java.util.List;
 import com.gospodarka.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
@@ -45,8 +49,9 @@ public class UserController {
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity<org.springframework.security.core.userdetails.User> login(){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<User> login(Principal principal){
+        User authenticatedUser = userRepository.findByLogin(principal.getName());
+        return ResponseEntity.ok().body(authenticatedUser);
     }
 
     @RequestMapping(value="/logmeout", method = RequestMethod.POST)
